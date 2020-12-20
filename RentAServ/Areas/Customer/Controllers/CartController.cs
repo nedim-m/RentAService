be5f78e77501499bd4ef936bbd1ca0d62ba165e5
nedim-m <nedim-m@users.noreply.github.com>
2020-12-20 21:22:13 +0100

    Shopping cart done

--- RentAServ/Areas/Customer/Controllers/CartController.cs
+++ RentAServ/Areas/Customer/Controllers/CartController.cs
@@ -15,31 +15,30 @@ namespace RentAServ.Areas.Customer.Controllers
     [Area("Customer")]
     public class CartController : Controller
     {
-        private readonly IUnitOfWork _unitOfwork;
+        private readonly IUnitOfWork _unitOfWork;
 
         [BindProperty]
         public CartViewModel CartVM { get; set; }
-        
+
         public CartController(IUnitOfWork unitOfWork)
         {
-            _unitOfwork = unitOfWork;
+            _unitOfWork = unitOfWork;
             CartVM = new CartViewModel()
             {
-                OrderHeader=new OrderHeader(),
-                ServiceList= new List<Service>()
+                OrderHeader = new Models.OrderHeader(),
+                ServiceList = new List<Service>()
             };
         }
 
         public IActionResult Index()
         {
-           if (HttpContext.Session.GetObject<List<int>>(SD.SessionCart) != null)
+            if (HttpContext.Session.GetObject<List<int>>(SD.SessionCart) != null)
             {
                 List<int> sessionList = new List<int>();
                 sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
-                foreach(int serviceId in sessionList)
+                foreach (int serviceId in sessionList)
                 {
-                    CartVM.ServiceList.Add(_unitOfwork.Service.GetFirstOrDefault(u => u.Id == serviceId,includeProperties:"Frequency,Category"));
-
+                    CartVM.ServiceList.Add(_unitOfWork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
                 }
             }
             return View(CartVM);
@@ -54,8 +53,7 @@ namespace RentAServ.Areas.Customer.Controllers
                 sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
                 foreach (int serviceId in sessionList)
                 {
-                    CartVM.ServiceList.Add(_unitOfwork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
-
+                    CartVM.ServiceList.Add(_unitOfWork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
                 }
             }
             return View(CartVM);
@@ -70,13 +68,14 @@ namespace RentAServ.Areas.Customer.Controllers
             {
                 List<int> sessionList = new List<int>();
                 sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+                CartVM.ServiceList = new List<Service>();
                 foreach (int serviceId in sessionList)
                 {
-                    CartVM.ServiceList.Add(_unitOfwork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
-
+                    CartVM.ServiceList.Add(_unitOfWork.Service.Get(serviceId));
                 }
             }
-            if (ModelState.IsValid)
+
+            if (!ModelState.IsValid)
             {
                 return View(CartVM);
             }
@@ -85,8 +84,8 @@ namespace RentAServ.Areas.Customer.Controllers
                 CartVM.OrderHeader.OrderDate = DateTime.Now;
                 CartVM.OrderHeader.Status = SD.StatusSubmitted;
                 CartVM.OrderHeader.ServiceCount = CartVM.ServiceList.Count;
-                _unitOfwork.OrderHeader.Add(CartVM.OrderHeader);
-                _unitOfwork.Save();
+                _unitOfWork.OrderHeader.Add(CartVM.OrderHeader);
+                _unitOfWork.Save();
 
                 foreach (var item in CartVM.ServiceList)
                 {
@@ -95,34 +94,32 @@ namespace RentAServ.Areas.Customer.Controllers
                         ServiceId = item.Id,
                         OrderHeaderId = CartVM.OrderHeader.Id,
                         ServiceName = item.Name,
-                        Price=item.Price
+                        Price = item.Price
                     };
-                    _unitOfwork.OrderDetails.Add(orderDetails);
-                    
 
-                   
+                    _unitOfWork.OrderDetails.Add(orderDetails);
+
                 }
-                _unitOfwork.Save();
+                _unitOfWork.Save();
                 HttpContext.Session.SetObject(SD.SessionCart, new List<int>());
-                return RedirectToAction("OrderConfrimation", "Cart", new { id = CartVM.OrderHeader.Id });
+                return RedirectToAction("OrderConfirmation", "Cart", new { id = CartVM.OrderHeader.Id });
             }
         }
 
-        public IActionResult OrderConfrimation(int id)
+        public IActionResult OrderConfirmation(int id)
         {
             return View(id);
         }
 
-        public IActionResult Remove(int id)
+
+        public IActionResult Remove(int serviceId)
         {
             List<int> sessionList = new List<int>();
             sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
-            sessionList.Remove(id);
-
+            sessionList.Remove(serviceId);
             HttpContext.Session.SetObject(SD.SessionCart, sessionList);
 
             return RedirectToAction(nameof(Index));
-
         }
     }
 }
